b1805b0af9fb0693ddfd9ae31317fe32869f7880
Juan Olave <olavejuan1805@gmail.com>
2024-04-17 18:05:14 -0500

    Arreglos 2

--- Entregable previo/js/ejercicio1.js
+++ Entregable previo/js/ejercicio1.js
@@ -10,19 +10,22 @@ for (let a = 1; a<=3; a++){
   if (!/^\d+$/.test(tarjeta) || tarjeta.length > 16) {
     alert("ERROR: Tarjeta no válida. Ingrese solo números y máximo 16 caracteres.");
   }
-  if (tarjeta === 0) {
-    break;
-  }
+
   else if (regex_express.test(tarjeta)) {
     alert("Tarjeta válida: American Express");
+    break;
   } else if (regex_diners.test(tarjeta)) {
     alert("Tarjeta válida: Diners Club");
+    break;
   } else if (regex_discover.test(tarjeta)) {
     alert("Tarjeta válida: Discover");
+    break;
   } else if (regex_mastercard.test(tarjeta)) {
     alert("Tarjeta válida: Mastercard");
+    break;
   } else if (regex_visa.test(tarjeta)) {
     alert("Tarjeta válida: Visa");
+    break;
   } else {
     alert("ERROR: Tarjeta no reconocida.");
   }
